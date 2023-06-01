6e9f224e1eb199a77461369d80f7177941dc7c45
Kirstine Peñalber <penalberkirstine@gmail.com>
2023-06-01 15:04:51 +0800

    feat: init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/**/*.tsx',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-        },
-        colors: {
-        }
-      },
-    },
-    plugins: [],
-  }
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
