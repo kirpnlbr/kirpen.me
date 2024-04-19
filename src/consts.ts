1a8f0f22ae901c125fe0a831e3da90f9dcb4645b
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-04-19 18:38:16 +0800

    fix: reduce spacing between name and title

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Kir Peñalber';
-export const SITE_DESCRIPTION = 'Design Engineer, Sophomore at Ateneo de Manila University';
+export const SITE_DESCRIPTION = 'Artist and Technologist, Sophomore at Ateneo de Manila University';
