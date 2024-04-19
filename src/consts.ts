939e74f79070b53c0505798cee8326e87e529fa0
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-04-19 17:25:34 +0800

    feat: add content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Kir Peñalber';
-export const SITE_DESCRIPTION = 'Deranged-flavored musings.';
+export const SITE_DESCRIPTION = 'Design Engineer, Sophomore at Ateneo de Manila University';
